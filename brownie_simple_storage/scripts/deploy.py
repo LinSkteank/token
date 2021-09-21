a8627be5bb74c1d326b31101a4f04024244c5c53
LinSkteank <80599444+LinSkteank@users.noreply.github.com>
2021-09-21 16:49:16 +0300

    brownie
    acc acca
    1234

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,5 +1,12 @@
+from brownie import accounts
 
+
+def deploy_simple_storage():
+    # account = accounts[0]
+    # print(account)
+    account = accounts.load("acca")
+    print(account)
 
 
 def main():
-    print("hello")
+    deploy_simple_storage()
